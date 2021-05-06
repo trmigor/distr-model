ebbe84f941660acc52a69a5956d1e2972b955e5b
trmigor <tarajmovich.i@phystech.edu>
2021-05-06 12:48:48 +0300

    Added package for user-defined contexts

---
+++ user/context/Context.go
@@ -0,0 +1,18 @@
+package context
+
+// Context represents user-defined context for a process.
+type Context interface{}
+
+// Common is a common context without any fields.
+type Common struct{}
+
+// SetX is a context for setting one value algorithm.
+type SetX struct {
+	X int
+}
+
+// Contexts is a map of all contexts available to processes.
+var Contexts = map[string]Context{
+	"Common": &Common{},
+	"SetX":   &SetX{},
+}
